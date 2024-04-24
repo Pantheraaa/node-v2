fbf85f411aabb9ae189357ddbbe25fdc5eab9492
Pantheraaa <rahul1210rk@gmail.com>
2024-04-24 15:52:42 +0530

    update: precendence handled for expression

--- src/calc/calc.service.ts
+++ src/calc/calc.service.ts
@@ -4,7 +4,7 @@ import { CalcDto } from './calc.dto';
 @Injectable()
 export class CalcService {
   calculateExpression(calcBody: CalcDto): number {
-    // check if expression is valid to evaluate:
+    // check if expression is valid to evaluate (neither starts nor ends with an operator):
     const isValid = this.identifyValidExpression(calcBody.expression);
     if (!isValid) throw new BadRequestException("Invalid expression provided");
 
@@ -18,25 +18,50 @@ export class CalcService {
     return isValidExpression;
   }
 
-  evaluateExpression(expression: string) {
+  evaluateExpression(expression: string): number {
     const arr = expression.split("");
-    const operands = ["+", "-", "*", "/"];
-    let operand: number;
-    let operator: string;
-    arr.forEach((opr) => {
-      if (operands.includes(opr)) return operator = opr;
+    const validOperands = ["+", "-", "*", "/"];
+    const operands: number[] = [];
+    const operators: string[] = [];
+    const precedence = {
+      '+': 1,
+      '-': 1,
+      '*': 2,
+      '/': 2
+    };
+
+    arr.forEach((ele) => {
+      // check if ele is operand:
+      if (!validOperands.includes(ele)) operands.push(parseInt(ele));
       else {
-        // if element is an operand and operand variable is empty:
-        if (!operand && !operator) return operand = parseInt(opr);
-        else {
-          // Perform evaluation when operand and operator already has value:
-          operand = this.calculateValues(operand, operator, parseInt(opr));
-          operator = undefined;
+        // if ele is an operator
+        while (operators.length > 0 && precedence[operators[operators.length - 1]] >= precedence[ele]) {
+          // Evaluate the top of the stack if the precedence of the current operator is lower or equal
+          const operator = operators.pop();
+          const rightOperand = operands.pop();
+          const leftOperand = operands.pop();
+          const result = this.calculateValues(leftOperand, operator, rightOperand);
+          operands.push(result);
         }
+
+        // store the operator into the operators stack:
+        operators.push(ele);
       }
     })
 
-    return operand;
+    // evaluate with the remaining operators in the stack:
+    while (operators.length > 0) {
+      const operator = operators.pop();
+      const rightOperand = operands.pop();
+      const leftOperand = operands.pop();
+      const result = this.calculateValues(leftOperand, operator, rightOperand);
+      operands.push(result);
+    }
+
+    if (operands.length !== 1) throw new Error("Invalid expression provided");
+
+    // return only operand left in the operands:
+    return operands.pop();
   }
 
   calculateValues(leftOperand: number, operator: string, rightOperand: number): number {
