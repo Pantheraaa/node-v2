38fc43650acf3a526c1d58e32ceb332355ed1a80
rahulplutos <rahul@plutos.one>
2024-04-24 11:22:15 +0530

    added: expression validator function

--- src/calc/calc.service.ts
+++ src/calc/calc.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CalcDto } from './calc.dto';
 
 @Injectable()
 export class CalcService {
   calculateExpression(calcBody: CalcDto) {
+    const isValid = this.identifyValidExpression(calcBody.expression);
+    if (!isValid) throw new BadRequestException("Invalid expression provided");
     return 0;
+  }
+
+  identifyValidExpression(expression: string): boolean {
+    const expressionPattern = /^[0-9][0-9+\-*/\s]*[0-9]$/;
+    const isValidExpression = expressionPattern.test(expression);
+    return isValidExpression;
   }
 }
